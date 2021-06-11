bcdc416a4b2203309e2eba0fd6a8c8a7bb5a9c53
Santiagodo <55608684+Santiagodo@users.noreply.github.com>
2021-06-10 19:49:40 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Login</title>
+    <link href="css/login.css" rel="stylesheet">
+  </head>
+  
+  <body>
+    
+    <section class="form-login">
+
+        <h5>Formulario Login</h5>
+
+        <input class="controls" type="text" name="usuario" value="" placeholder="Usuario">
+
+        <input class="controls" type="password" name="contrasena" value="" placeholder="Contraseña">
+        <br>
+       <center> <a href="views/usuarios/index.php" class="btn btn-info" role="button">acceder</a></center>
+       <br>
+        <p><a href="">¿Olvidastes la Contraseña?
+       
+    
+</body>
+</html>
+
+
