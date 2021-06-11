7dbbef4a332d3f0d22692c04607026541a3a024d
Santiagodo <55608684+Santiagodo@users.noreply.github.com>
2021-06-10 19:53:31 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,31 +1,95 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
+<?php require '../../includes/_db.php' ?>
+<?php require '../../includes/_header.php' ?>
 
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Login</title>
-    <link href="css/login.css" rel="stylesheet">
-  </head>
-  
-  <body>
-    
-    <section class="form-login">
+<div id= "content">
+        <section>
+        <div class="container mt-5">
+<div class="row">
+<div class="col-sm-12 mb-3">
+<center><h1>Usuarios</h1></center>
+</div>
+<div class="col-sm-12">
+<div class="table-responsive">
 
-        <h5>Formulario Login</h5>
 
-        <input class="controls" type="text" name="usuario" value="" placeholder="Usuario">
+<table class="table table-striped table-hover">
+<thead>
 
-        <input class="controls" type="password" name="contrasena" value="" placeholder="Contraseña">
-        <br>
-       <center> <a href="views/usuarios/index.php" class="btn btn-info" role="button">acceder</a></center>
-       <br>
-        <p><a href="">¿Olvidastes la Contraseña?
-       
-    
-</body>
+<tr>
+<th>Nombre</th>
+<th>Telefono</th>
+<th>Correo</th>
+<th>Acciones</th>
+
+
+</tr>
+
+</thead>
+
+<tbody>
+
+<?php
+
+$sql = "SELECT id, nombre, telefono, correo FROM usuarios";
+$usuarios = mysqli_query($conexion, $sql);
+if($usuarios -> num_rows > 0){
+foreach($usuarios as $key => $row ){
+
+
+
+
+?>
+
+<tr>
+<td><?php echo $row['nombre']; ?></td>
+<td><?php echo $row['telefono']; ?></td>
+<td><?php echo $row['correo']; ?></td>
+<td>
+  <a href="editar.php?id=<?php echo $row['id']?>">
+    <div">
+      Editar
+    </div>
+  </a>
+  <a>|</a>
+  <a href="eliminar.php?id=<?php echo $row['id']?>">
+    <div">
+    Eliminar
+    </div>
+  </a>
+</td>
+</tr>
+
+
+<?php
+}
+}else{
+
+    ?>
+    <tr class="text-center">
+    <td colspan="4">No existen usuarios </td>
+    </tr>
+
+    <?php
+}?>
+</tbody>
+
+</table>
+</div>
+</div>
+</div>
+</div>
+        </section>
+
+
+        <section>
+            <div class= "container">
+                <div class= "row">
+                    <div class= "col-lg-9">
+            </div>
+        </section>
+    </div>
+    <?php require '../../includes/_footer.php' ?>
 </html>
-
-
